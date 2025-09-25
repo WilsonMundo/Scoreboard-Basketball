04477561535fe8ef80a0bcf7dc75db8be25d84a0
WilsonMundo <Wilsonmundoa@outlook.com>
2025-09-24 22:34:52 -0600

    remover campo de nit no es parte del proceso

--- AngularApp1.Server/Application/DTO/Auth/UserRegister.cs
+++ AngularApp1.Server/Application/DTO/Auth/UserRegister.cs
@@ -10,8 +10,6 @@ namespace AngularApp1.Server.Application.DTO.Auth
         [Required(ErrorMessage = "Este campo {0} es requerido")]
         public string name { get; set; } = null!;
         public string? direccion { get; set; }
-        [Required(ErrorMessage = "Este campo {0}  es requerido")]
-        public string nit { get; set; } = null!;
         [Required(ErrorMessage = "Este campo {0} es requerido")]
         public string telefono { get; set; } = null!;
     }
